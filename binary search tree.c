d0350911be52d39c35fcff33aad27ad2ade5f973
DESKTOP-KAF5LK8\ASUS-PC <ken83715@gmail.com>
2017-12-19 23:12:06 +0800

    20171219

--- binary search tree.c
+++ binary search tree.c
@@ -4,7 +4,6 @@
 typedef struct listNode *listPointer;
 typedef struct listNode{
 	int data;
-	listPointer parent;
 	listPointer left;
 	listPointer right;
 } node;
@@ -15,13 +14,12 @@ listPointer creatnode()
 	newnode = (listPointer)malloc(sizeof(node));
 	newnode->left = NULL;
 	newnode->right = NULL;
-	newnode->parent = NULL;
 	return newnode;
 }
 
 void add(listPointer n, int number)
 {
-	if(number < n->data)
+	if(number > n->data)
 	{
 		if(n->left != NULL)
 			add(n->left, number);		
@@ -30,7 +28,6 @@ void add(listPointer n, int number)
 			listPointer newnode = creatnode();
 			newnode->data = number;
 			n->left = newnode;
-			newnode->parent = n;
 		}
 	}
 	else
@@ -42,21 +39,20 @@ void add(listPointer n, int number)
 			listPointer newnode = creatnode();
 			newnode->data = number;
 			n->right = newnode;
-			newnode->parent = n;
 		}
 	}
 }
 
 int search(listPointer n, int number)
 {
-	if(n->data > number)
+	if(number > n->data)
 	{
 		if(n->left != NULL)
 			return search(n->left, number);
 		else
 			return NULL;
 	}
-	else if(n->data < number)
+	else if(number < n->data)
 	{
 		if(n->right != NULL)
 			return search(n->right, number);		
@@ -69,26 +65,26 @@ int search(listPointer n, int number)
 
 void print(listPointer p, int level)
 {
+	if(p->right != NULL)
+		print(p->right, level + 1);	
+		int i;
+	for(i = 0; i < level - 1; i++)
+		printf("\t");
+	printf("%d\n", p->data);
 	if(p->left != NULL)
 		print(p->left, level + 1);
-		int i;
-	for(i = 0; i < level; i++)
-		printf("\t");
-	printf("%d\n",p->data);
-	if(p->right != NULL)
-		print(p->right, level + 1);
 }
 
 int main()
 {
 	listPointer root = NULL;
-	
 	int run = 1;
 	int f = 0;
 	int data, n;
+	
 	while(run == 1)
 	{
-		printf("1.新增資料 2.搜尋資料 3.印出 請選擇:\n");
+		printf("1.新增資料 2.搜尋資料 3.印出 4.離開 請選擇:\n");
 		scanf("%d", &f);
 		switch(f)
 		{
@@ -104,20 +100,25 @@ int main()
 					add(root, data);
 				break;
 			case 2:
+				if(root == NULL)
+				{
+					printf("Empty!\n");
+					break;
+				}
 				printf("請輸入要搜尋的資料:\n");
 				scanf("%d", &data);
-				if(root == NULL)
+				n = search(root, data);
+				if(n == NULL)
 					printf("查無資料\n");
 				else
-					n = search(root, data);
-					if(n == NULL)
-						printf("查無資料\n");
-					else
-						printf("找到%d\n", n);
+					printf("找到%d\n", n);
 				break;
 			case 3:
 				print(root, 1);
 				printf("\n\n");
+				break;
+			case 4:
+				run = 0;
 				break;
 		}
 	}
